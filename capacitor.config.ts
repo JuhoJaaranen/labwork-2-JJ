f956e99bd35e45a7e6640763e3a4c6efc16e03f7
JuhoJaaranen <juho.jaaranen@gmail.com>
2025-09-29 21:30:43 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'labwork-2-App',
+  webDir: 'dist'
+};
+
+export default config;
